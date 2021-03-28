9ef052589ec5c060accc982405efd1e9491814be
Damon P. Cortesi <d.lifehacker@gmail.com>
2021-03-27 22:23:54 -0700

    Add initial  YouTube channel support

--- crates/service/youtube.py
+++ crates/service/youtube.py
@@ -13,7 +13,51 @@ def endpoint(path):
 
 
 def get(url, params={}):
+    params.update({"key": os.getenv("YOUTUBE_API_KEY")})
     return requests.get(url, params=params)
+
+
+def _get_upload_playlist_id(channel_id):
+    params = {
+        "part": "contentDetails",
+        "id": channel_id,
+    }
+    r = get(endpoint("channels"), params)
+    return (
+        r.json()
+        .get("items")[0]
+        .get("contentDetails")
+        .get("relatedPlaylists")
+        .get("uploads")
+    )
+
+
+def _get_playlist_video_ids(playlist_id):
+    params = {
+        "part": "snippet",
+        "playlistId": playlist_id,
+    }
+    # TODO: Pagination
+    r = get(endpoint("playlistItems"), params)
+    video_ids = []
+
+    for item in r.json().get("items", []):
+        video_ids.append(item.get("snippet").get("resourceId").get("videoId"))
+
+    return video_ids
+
+
+def channel_videos(channel_id):
+    """
+    Retrieves all videos from a specific channel.
+    This requires us to:
+        - get the "upload" playlist
+        - get a list of the videos in that playlist
+        - fetch the stats for each video
+    """
+    upload_id = _get_upload_playlist_id(channel_id)
+    video_ids = _get_playlist_video_ids(upload_id)
+    return videos(video_ids)
 
 
 def videos(video_ids):
@@ -25,14 +69,13 @@ def videos(video_ids):
     params = {
         "part": "snippet,contentDetails,statistics",
         "id": video_ids,
-        "key": os.getenv("YOUTUBE_API_KEY"),
     }
 
     r = get(endpoint("videos"), params)
     return r.json().get("items", [])
 
 
-SUPPORTED_CMDS = ["videos"]
+SUPPORTED_CMDS = ["videos", "channel_videos"]
 
 if __name__ == "__main__":
     # Ensure a valid command is provided
@@ -45,6 +88,10 @@ if __name__ == "__main__":
     if cmd == "videos":
         video_ids = options[0].split(",")
         result = videos(video_ids)
+
+    if cmd == "channel_videos":
+        channel_id = options[0]
+        result = channel_videos(channel_id)
 
     for r in result:
         print(json.dumps(r))

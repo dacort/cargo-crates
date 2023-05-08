ae21e64b9c09f638660b28a609fad5aaf9e86a5e
Damon P. Cortesi <d.lifehacker@gmail.com>
2023-05-08 11:25:11 -0700

    Add Slack history

--- crates/service/slack.py
+++ crates/service/slack.py
@@ -1,3 +1,5 @@
+import datetime as dt
+import time
 import json
 import os
 import sys
@@ -7,20 +9,21 @@ import requests
 ENV_VARS = ["SLACK_TOKEN", "SLACK_COOKIE_TOKEN"]
 API_BASE = "slack.com/api"
 
+
 def endpoint(path):
     return f"https://{API_BASE}/{path}"
 
 
 def get(url, params={}):
     cookies = {}
-    if os.getenv('SLACK_TOKEN').startswith("xoxc"):
-        cookies['d'] = os.getenv('SLACK_COOKIE_TOKEN')
+    if os.getenv("SLACK_TOKEN").startswith("xoxc"):
+        cookies["d"] = os.getenv("SLACK_COOKIE_TOKEN")
 
     return requests.get(
         url,
         params=params,
         headers={"Authorization": f"Bearer {os.getenv('SLACK_TOKEN')}"},
-        cookies=cookies
+        cookies=cookies,
     )
 
 
@@ -63,7 +66,36 @@ def channels():
             break
 
 
-SUPPORTED_CMDS = ["search", "channels"]
+def history(channel_name, days=7, include_threads=True):
+    """
+    Returns a list of messages for a specific channel in the given timeframe (now-timeframe).
+    Optionally includes thread messages by default.
+    """
+    chan_id = None
+    for chan in channels():
+        if chan["name"].lower() == channel_name.lower():
+            chan_id = chan["id"]
+            break
+
+    if not chan_id:
+        raise RuntimeError(f"Could not find channel '{channel_name}'")
+
+    oldest = dt.datetime.utcnow() - dt.timedelta(days=days)
+    params = {"channel": chan_id, "oldest": time.mktime(oldest.timetuple())}
+    while True:
+        r = get(endpoint("conversations.history"), params)
+        data = r.json()
+        for message in data.get("messages", []):
+            yield message
+        # Next cursor is by default an empty string, but in case it's not use that as the default.
+        cursor = data.get("response_metadata", {}).get("next_cursor", "")
+        if cursor > "":
+            params["cursor"] = cursor
+        else:
+            break
+
+
+SUPPORTED_CMDS = ["search", "channels", "history"]
 
 if __name__ == "__main__":
     cmd = "search"
@@ -84,6 +116,13 @@ if __name__ == "__main__":
 
     if cmd == "channels":
         result = channels()
+
+    if cmd == "history":
+        channel_name = sys.argv[2]
+        timeframe = 1
+        if len(sys.argv) > 3:
+            timeframe = int(sys.argv[3])
+        result = history(channel_name, timeframe)
 
     for r in result:
         r["team_name"] = team_name
